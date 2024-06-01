9453cc4f161a91731d8a77c04cf4f3ba916a514d
Tohirbek04 <tohirbekakhmedov1310@gmail.com>
2024-06-01 17:05:02 +0500

    commit

--- apps/permissions.py
+++ apps/permissions.py
@@ -5,5 +5,5 @@ from apps.models import Vacancy
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj: Vacancy):
-        return request.user == obj.user
+        return request.user == obj.user and request.user.is_authenticated
 
